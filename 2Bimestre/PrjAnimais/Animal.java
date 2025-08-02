dc7192c260b767d51f8183fc053ce3ae28eab9a3
Liv288 <liveira026@gmail.com>
2025-08-02 19:45:21 -0300

    Add files via upload

---
+++ 2Bimestre/PrjAnimais/Animal.java
@@ -0,0 +1,45 @@
+
+public class Animal {
+	String nome;
+	double peso;
+	double altura;
+	String habitat;
+	String Caracteristicas;
+	String Curiosidades;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public double getAltura() {
+		return altura;
+	}
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public String getCaracteristicas() {
+		return Caracteristicas;
+	}
+	public void setCaracteristicas(String caracteristicas) {
+		Caracteristicas = caracteristicas;
+	}
+	public String getCuriosidades() {
+		return Curiosidades;
+	}
+	public void setCuriosidades(String curiosidades) {
+		Curiosidades = curiosidades;
+	}
+}
