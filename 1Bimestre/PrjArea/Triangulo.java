e9e44aed7389af8bce9a970a2387d9ed02624a1e
Liv288 <liveira026@gmail.com>
2025-08-02 19:36:16 -0300

    Add files via upload

---
+++ 1Bimestre/PrjArea/Triangulo.java
@@ -0,0 +1,8 @@
+
+public class Triangulo extends Propriedade{
+	public void Area(){
+		double area = (getBase() * getAltura())/2;
+		
+		System.out.println("A área do triângulo é igual a " +area+ "cm²");
+	}
+}
