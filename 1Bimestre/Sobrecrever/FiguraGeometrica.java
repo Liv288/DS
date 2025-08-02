939ad232c1bc592b22099d717f499b9b6d40abb3
Liv288 <liveira026@gmail.com>
2025-08-02 19:38:54 -0300

    Add files via upload

---
+++ 1Bimestre/Sobrecrever/FiguraGeometrica.java
@@ -0,0 +1,6 @@
+
+public class FiguraGeometrica {
+	public void calcularArea() {
+	 System.out.println("CalcularArea");
+ }
+}
---
+++ 1Bimestre/Sobrecrever/Quadrado.java
@@ -0,0 +1,7 @@
+public class Quadrado extends FiguraGeometrica {
+	
+	public void calcularArea() {
+		super.calcularArea();
+		 System.out.println("CalcularAreaQuadrado");
+	 }
+}
