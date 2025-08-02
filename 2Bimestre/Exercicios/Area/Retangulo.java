ed5546583bcb2ad28c2566862cf974a0f60aa725
Liv288 <liveira026@gmail.com>
2025-08-02 19:42:41 -0300

    Add files via upload

---
+++ 2Bimestre/Exercicios/Area/Retangulo.java
@@ -0,0 +1,8 @@
+
+public class Retangulo extends Propriedade{
+	public void Area() {
+		double area = getBase() * getAltura();
+		
+		System.out.println("A área do retângulo é igual a "+area+"cm²");
+	}
+}
