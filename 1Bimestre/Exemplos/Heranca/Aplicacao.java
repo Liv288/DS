05a37fc63e4738ccc5968d6d3c38e959beb953a4
Liv288 <liveira026@gmail.com>
2025-08-02 19:33:31 -0300

    Add files via upload

---
+++ 1Bimestre/Exemplos/Heranca/Aplicacao.java
@@ -0,0 +1,10 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+	
+		Professor prof = new Professor();
+
+	}
+
+}
---
+++ 1Bimestre/Exemplos/Heranca/Pessoa.java
@@ -0,0 +1,28 @@
+
+public class Pessoa {
+
+	private String Matricula;
+	private String estado;
+	private String dataCriacao;
+	
+	public String getMatricula() {
+		return Matricula;
+	}
+	public void setMatricula(String matricula) {
+		Matricula = matricula;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(String dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	
+	
+}
